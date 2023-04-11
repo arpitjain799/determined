11bc262270e5adb7e976bc939757e96fe9665452
Carolina Calderon <caroelizcalderon@gmail.com>
2023-04-11 13:59:49 -0400

    Revert "chore: replace helm pw-changer image with generic det env image (#6483)"
    This reverts commit 0c6bfad257369a1ba2501a559f6c31aff2bbc90b.

--- helm/charts/determined/scripts/k8s-password-change.py
+++ helm/charts/determined/scripts/k8s-password-change.py
@@ -2,24 +2,29 @@ import json
 import sys
 import time
 from typing import List
-import requests
+
+from determined_common import api
+from kubernetes import client, config
 
 
 def pwChange(user: str, password: str, entrypoint: str) -> None:
     print(f"Trying to change {user}'s password", flush=True)
     while True:
         try:
-            resp = requests.post(
-                url=f"http://{entrypoint}/api/v1/auth/login",
-                json={"username": user, "password": ""},
-                verify=False,
+            resp = api.post(
+                entrypoint,
+                "/api/v1/auth/login",
+                body={"username": user, "password": ""},
+                authenticated=False,
             )
+
             data = json.loads(resp.text)
-            requests.post(
-                url=f"http://{entrypoint}/api/v1/users/{data['user']['id']}/password",
-                json=password,
+            api.post(
+                entrypoint,
+                f"/api/v1/users/{data['user']['id']}/password",
+                body=password,
                 headers={"Authorization": f"Bearer {data['token']}"},
-                verify=False,
+                authenticated=False,
             )
             print(f"Sucessfully changed {user}'s password", flush=True)
             return
@@ -31,55 +36,37 @@ def pwChange(user: str, password: str, entrypoint: str) -> None:
 def checkPortAlive(entrypoint: str) -> None:
     while True:
         try:
-            requests.get(
-                url=f"http://{entrypoint}/api/v1/master",
-                verify=False,
-            )
+            api.get(entrypoint, "/api/v1/master", authenticated=False)
             return
         except Exception as e:
             print(f"Encountered exception: {e}")
             continue
 
 
-def getMasterAddress(
-    namespace: str,
-    service_name: str,
-    master_port: str,
-    node_port: str,
-    service_host: str,
-    service_port: str,
-    token: str,
-) -> str:
-
+def getMasterAddress(namespace: str, service_name: str, master_port: str, node_port: str) -> str:
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
     target_service = f"determined-master-service-{service_name}"
 
     if node_port != "true":
         while True:
-            services = requests.get(
-                url=f"https://{service_host}:{service_post}/api/v1/namespaces/{namespace}/services",
-                headers={"Authorization": f"Bearer {token}"},
-                verify=False,
-            ).json()
-            for svc in services["items"]:
-                if target_service in svc["metadata"]["name"]:
-                    status = svc["status"]["loadBalancer"]
-                    if "ingress" not in svc["status"]["loadBalancer"] or status["ingress"] is None:
+            services = v1.list_namespaced_service(namespace)
+            for svc in services.items:
+                if target_service in svc.metadata.name:
+                    ingress = svc.status.load_balancer.ingress
+                    if ingress is None:
                         time.sleep(1)
                         break
-                    if status["ingress"][0].get("hostname"):
+                    if ingress[0].hostname is not None:
                         # use hostname over ip address, if available
-                        return f"{status['ingress'][0]['hostname']}:{master_port}"
+                        return f"{ingress[0].hostname}:{master_port}"
                     else:
-                        return f"{status['ingress'][0]['ip']}:{master_port}"
+                        return f"{ingress[0].ip}:{master_port}"
 
-    services = requests.get(
-        url=f"https://{service_host}:{service_post}/api/v1/namespaces/{namespace}/services",
-        headers={"Authorization": f"Bearer {token}"},
-        verify=False,
-    ).json()
-    for svc in services["items"]:
-        if target_service in svc["metadata"]["name"]:
-            entrypoint = f"{svc['spec']['cluster_ip']}:{master_port}"
+    services = v1.list_namespaced_service(namespace)
+    for svc in services.items:
+        if target_service in svc.metadata.name:
+            entrypoint = f"{svc.spec.cluster_ip}:{master_port}"
             checkPortAlive(entrypoint)
 
             return entrypoint
@@ -87,16 +74,16 @@ def getMasterAddress(
 
 
 def main(argv: List[str]) -> None:
-    if len(argv) < 7:
+    if len(argv) < 5:
         raise Exception("not enough args")
-    if argv[7] == "":
+    if argv[4] == "":
         raise Exception("no password supplied")
 
     for i in range(len(argv)):
         argv[i] = argv[i].strip()
-    addr = getMasterAddress(argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6])
-    pwChange("determined", argv[7], addr)
-    pwChange("admin", argv[7], addr)
+    addr = getMasterAddress(argv[0], argv[1], argv[2], argv[3])
+    pwChange("determined", argv[4], addr)
+    pwChange("admin", argv[4], addr)
 
 
 if __name__ == "__main__":
